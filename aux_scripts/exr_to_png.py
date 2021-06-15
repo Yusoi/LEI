d253e813d9897084d1197a25d3b8a6dc237120e0
Bernardo Silva <bernardosilva.business@gmail.com>
2021-06-15 23:07:58 +0100

    Scripts

---
+++ aux_scripts/exr_to_png.py
@@ -0,0 +1,37 @@
+import argparse
+import tensorflow as tf
+import cv2
+import numpy as np
+import glob
+
+#png_to_exr.py "C:\\Users\\Bernardo Silva\\Desktop\\LEI\\Optix\\build\\Release\\generated_dataset\\" "C:\\Users\\Bernardo Silva\\Desktop\\LEI\\aux_scripts\\exr\\" "noisy"
+#DATASET_PATH = "C:\\Users\\Bernardo Silva\\Desktop\\LEI\\Optix\\build\\Release\\generated_dataset\\"
+#OUTPUT_PATH = "C:\\Users\\Bernardo Silva\\Desktop\\LEI\\aux_scripts\\exr\\"
+
+def load_files(img):
+    image = cv2.imread(img,cv2.IMREAD_UNCHANGED)
+    image=image*65535
+    image[image>65535]=65535
+    image = np.uint16(image)
+    img = img.split("\\")[-1]
+    img = img.split("_")[0]
+    img = OUTPUT_PATH+img+"_"+EXTENSION+".png"
+    print(img)
+    cv2.imwrite(img,image)
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Loads png images and loads them into exr files")
+    parser.add_argument("i",type=str)
+    parser.add_argument("o",type=str)
+    parser.add_argument("ext",type=str)
+    args = parser.parse_args()
+
+    INPUT_PATH = args.i
+    OUTPUT_PATH = args.o
+    EXTENSION = args.ext
+
+    file_list = glob.glob(INPUT_PATH+"*_"+EXTENSION+".exr")
+    print(file_list)
+    for file in file_list:
+        load_files(file)
--- aux_scripts/run_denoiser.py
+++ aux_scripts/run_denoiser.py
@@ -1,9 +1,9 @@
-import subprocess
-
-file_list = glob.glob(INPUT_PATH+"*_noisy.exr")
+import os
+import glob
+import argparse
 
 if __name__ == "__main__":
-    command = "C:\\ProgramData\\NVIDIA Corporation\\OptiX SDK 7.3.0\\SDK\\build\\bin\\Release\\optixDenoiser.exe"
+    command = "\"C:\\ProgramData\\NVIDIA Corporation\\OptiX SDK 7.3.0\\SDK\\build\\bin\\Release\\optixDenoiser.exe\""
 
     parser = argparse.ArgumentParser(description="Loads png images and loads them into exr files")
     parser.add_argument("i",type=str)
@@ -13,6 +13,12 @@ if __name__ == "__main__":
     INPUT_PATH = args.i
     OUTPUT_PATH = args.o
 
-    file_list = glob.glob(INPUT_PATH+"*_noisy.png")
+    file_list = glob.glob(INPUT_PATH+"*_noisy.exr")
     for file in file_list:
-        load_files(file)
+        nr = file.split("\\")[-1]
+        nr = nr.split("_")[0]
+        com = command+" -o \""+OUTPUT_PATH+nr+"_prediction.exr\" " + "\""+file+"\""
+        print(com)
+        stream = os.popen(com)
+        output = stream.read()
+        print(output)
